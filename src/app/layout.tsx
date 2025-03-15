e4eaebf42c82ec6b95dbe12bfa50610dc00deeec
naeimDz <naeimdz1703@gmail.com>
2025-03-15 04:52:22 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 
+import I18nProvider from "@/i18n/provider";
 import "./globals.css";
+import { Locale } from "@/i18n/config";
 
 export default function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: Locale };
+
 }>) {
   return (
     <html>
-      <body>
+      <body >
+      <I18nProvider locale={params.locale}>{children}</I18nProvider>
         {children}
       </body>
     </html>
