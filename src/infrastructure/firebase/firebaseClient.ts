186b85543756b20aac2544637a4229e42af486d6
naeimDz <naeimdz1703@gmail.com>
2025-03-15 03:58:13 +0100

    🔥 إعداد Firebase Client وإضافة Firestore و Auth

---
+++ src/infrastructure/firebase/firebaseClient.ts
@@ -0,0 +1,13 @@
+// infrastructure/firebase/firebaseClient.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from './config';
+import firebase from 'firebase/compat/app';
+
+
+const app = !firebase.apps.length ? initializeApp(firebaseConfig) : firebase.app();
+
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+export default app;
